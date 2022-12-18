a57dff0b3dbc21f7710f9bff2378c89482820e90
Peter V <98245483+p5quared@users.noreply.github.com>
2022-12-18 15:04:40 -0500

    +layout.svelte - default styles

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    // body
+    import "@fontsource/catamaran"
+    import "@fontsource/comfortaa"
+
+    // script
+    import "@fontsource/comforter"
+    import "@fontsource/comforter-brush"
+
+    import "./reset.css"
+</script>
+
+<slot />
+
+<style>
+    :root {
+        --font-body: "Catamaran", sans-serif;
+        --font-script: "Comforter Brush", cursive;
+        --color-body: #4F4F4F;
+        --color-secondary: #769676;
+
+        --color-background: #FAEBD7;
+        --color-background-2: #4F4F4F;
+
+        background-color: var(--color-background);
+        color: var(--color-body);
+
+        font-family: var(--font-body);
+        font-size: 24px;
+    }
+</style>
