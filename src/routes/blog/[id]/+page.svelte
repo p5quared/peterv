07ca0d4ea29f4b448b97a5446628439eef949146
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-07-16 19:02:54 -0400

    setup blog
    connect to pocketbase on fly
    
    still needs styles

---
+++ src/routes/blog/[id]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+
+import type { PageData } from './$types'
+export let data: PageData
+const {post} = data
+
+</script>
+
+<div class="container px-12">
+<a href="/home" class="anchor">home</a>
+<a href="/blog" class="anchor">return</a>
+	<h1 class="h1">{post.title}</h1>
+	{@html post.content}
+</div>
