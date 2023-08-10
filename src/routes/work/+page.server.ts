48c32a122407c24e392845b1dea1b4a2d414b3de
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-08-09 23:37:30 -0400

    /work
    create /work page. not positive I like how similar it is to /blog. I literally just copied it over

---
+++ src/routes/work/+page.server.ts
@@ -0,0 +1,20 @@
+import type { PageServerLoad } from './$types'
+import { createClient } from '@sanity/client'
+import { error } from '@sveltejs/kit'
+
+const client = createClient({
+	projectId: 'x4wuwagv',
+	dataset: 'production',
+	apiVersion: '2021-10-21',
+	useCdn: true,
+});
+
+
+export const load = ( async () => {
+	const posts = await client.fetch(`*[_type =="projects"] | order(_createdAt desc)`);
+
+	if (posts) {
+		return { posts };
+	}
+	throw error(500, 'Internal Server Error')
+}) satisfies PageServerLoad
