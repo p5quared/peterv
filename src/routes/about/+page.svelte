a54f3d4facf3d573012bcad74e4c2e86a5dd1320
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-08-19 15:18:28 -0400

    add/rewrite /about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,38 +11,48 @@ import headshot from '$lib/assets/headshot2.jpg';
 
 
 <div class="container">
-<a href={resume} target="_blank" class="anchor mb-12 text-tertiary-600 italic">Click here for a more formal (professional) description of me.</a>
+<a href={resume} target="_blank" class="anchor mb-12 text-tertiary-600 italic">Click for resume.</a>
 <article class="prose">
 	<h1 class="h1">About</h1>
 	<Avatar src={headshot}
-	class="float-left mr-8 mb-4"
+	class="float-left m-4"
 	width="w-32"
 	rounded="rounded-full"/>
 	<p>Howdy, Peter here. I'm a full time student studying computer science and math.
-	What I enjoy most about programming perhaps is the idea of simplicity--not
-	that programming is simple, but I love taking complex ideas and breaking them
-	into digestible pieces.
+	I love programming because I love the freedom of tinkering and going down the rabbit
+	hole on things many would consider a waste of time.
+	I could probably be happy with VSCode out of the box--I'd rather
+	spend 20 hours configuring Neovim. The better Apple Music player 'Cider' has stopped
+	supporting Mac--why don't I just create my own player? To me, learning to program feels
+	like practicing self-reliance which is enlightening and fulfilling.
 	</p>
 	<p>
 	Born and raised in a tiny town (re: no traffic lights, gas, or groceries) in the
 	Adirondack mountains of upstate New York, I found wilderness first and computers second.
-	One of my earliest excitements with computers was in middle school when I figured out
-	you could access an app called "Remote Desktop" through Command Prompt and use it to shutdown
-	any computer on the network at will. I was hooked.
+	</p>
+	<p>Perhaps my earliest memory of programming was in high-school when I started
+	messing around with bash scripts. One day I found that I could access the remote-control
+	software for controlling the school's computer's on the network via CMD. Immediately, I began
+	slowly gathering the network ID's of every computer I could. After about a week of cataloging,
+	I now had the ability to shut down anybody's computer at will, and even attach messages ranging
+	from the miscellaneous 'Required updates must be installed: computer will restart in 8 seconds',
+	to the mischevious 'This is the FBI, your data has been compromised: beginning complete system
+	wipe. If you tell anyone we will arrest you'.
 	</p>
 	<p>
-	Over the past few years I have worn many hats, after dropping out of RPI to pursue a startup idea
-	I decided to launch a boat rental business, boatADK, which I ran for a few years before
-	selling it in 2023. I also had a stint working at one of the most exclusive boutique hotels
-	in the country, The Point, where, as Property Host, I was responsible for the guest experience
-	and trained towards sommoliere-ship. I've decided I love programming more than wine, so I'm back
-	in school planning to graduate with a degree in computer science and pure math in Spring '25.
+	Over the past few years I have worn many hats. After dropping out of RPI to pursue a startup idea
+	I decided to launch a boat rental business, boatADK. I ran this, renting pontoon boats to people on
+	lake placid for a few years before selling it in 2023.
+	I also had a stint working at one of the most exclusive boutique hotels in the country, The Point,
+	where, as Property Host, I was responsible for the guest experience and trained towards sommoliere-ship.
+	I've decided I love programming more than wine, so I'm back in school planning to graduate with a degree
+	in computer science and pure math in Spring '25.
 	</p>
 	
 	{#if browser}
 		<p>
 		If you are not a robot and want to get in touch you can reach me here:
-		petervaiciulis@gmail.com.
+		<a href="mailto:petervaiciulis@gmail.com">petervaiciulis@gmail.com</a>.
 		</p>
 	{:else}
 		<noscript>
