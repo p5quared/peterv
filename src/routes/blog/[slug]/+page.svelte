d821be5062485536890d4fe15d005919aceaee41
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-09-21 20:39:49 -0400

    black text + refactor

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,30 +1,17 @@
 <script lang="ts">
-import type { PageData } from "./$types";
-export let data: PageData
-const { post } = data
+	import type { PageData } from "./$types";
+	import PortableRenderer from "$lib/PortableRenderer.svelte";
 
-import {PortableText} from '@portabletext/svelte'
-import PortableImage from "$lib/PortableImage.svelte";
 
-import PortableAnchor from "$lib/PortableAnchor.svelte";
-import PortableBlockQuote from "$lib/PortableBlockQuote.svelte";
-console.log(post.body)
+
+	export let data: PageData
+	const { post } = data
+
+// console.log(post.body)
 </script>
 
 <h1 class="h1">{post.title}</h1>
-<article class="prose [&>p]:leading-relaxed">
-	<PortableText value={post.body}
-	components={{
-		types: {
-		image: PortableImage,
-		},
-		marks: {
-		link: PortableAnchor,
-		},
-		block: {
-		blockquote: PortableBlockQuote,
-		},
-	}}
-	/>
+<article class="prose [&>p]:leading-relaxed [&>*]:text-black">
+	<PortableRenderer value={post.body} />
 	<p>-Peter V.</p>
 </article>
--- src/routes/work/[slug]/+page.svelte
+++ src/routes/work/[slug]/+page.svelte
@@ -1,28 +1,14 @@
 <script lang="ts">
 import type { PageData } from "./$types";
-export let data: PageData
-const { work } = data
+import PortableRenderer from "$lib/PortableRenderer.svelte";
 
-import {PortableText} from '@portabletext/svelte'
-import PortableImage from "$lib/PortableImage.svelte";
-	import PortableAnchor from "$lib/PortableAnchor.svelte";
-	import PortableBlockQuote from "$lib/PortableBlockQuote.svelte";
+
+	export let data: PageData
+	const { work } = data
 </script>
 
 <h1 class="h1">{work.title}</h1>
-<article class="prose [&>p]:leading-relaxed">
-	<PortableText value={work.body}
-	components={{
-		types: {
-		image: PortableImage,
-		},
-		marks: {
-		link: PortableAnchor,
-		},
-		block: {
-		blockquote: PortableBlockQuote,
-		},
-	}}
-	/>
+<article class="prose [&>p]:leading-relaxed [&>*]:text-black">
+	<PortableRenderer value={work.body} />
 	<p>-Peter V.</p>
 </article>
