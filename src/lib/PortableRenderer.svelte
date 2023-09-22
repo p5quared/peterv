36dbc96d529bd462831a46b0d1921640fb3a4719
Peter Vaiciulis <petervaiciulis@gmail.com>
2023-09-21 20:39:37 -0400

    PortableRenderer
    -- componenet to singly manage the rendering of all prose from sanity

---
+++ src/lib/PortableRenderer.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { InputValue } from "@portabletext/svelte/ptTypes";
+
+	import { PortableText } from "@portabletext/svelte";
+
+	import PortableImage from "./PortableImage.svelte";
+	import PortableAnchor from "./PortableAnchor.svelte";
+	import PortableBlockQuote from "./PortableBlockQuote.svelte";
+
+	export let value: InputValue | undefined;
+</script>
+
+<PortableText {value}
+	components={{
+		types: {
+		image: PortableImage,
+		},
+		marks: {
+		link: PortableAnchor,
+		},
+		block: {
+		blockquote: PortableBlockQuote,
+		},
+	}}
+/>
