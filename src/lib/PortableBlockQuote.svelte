659ad013f8798283d2584816fd1f149feeb2e446
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-02-02 19:17:51 -0500

    superfluous log

--- src/lib/PortableBlockQuote.svelte
+++ src/lib/PortableBlockQuote.svelte
@@ -1,11 +1,3 @@
-<script lang="ts">
-import type { BlockComponentProps } from "@portabletext/svelte";
-export let portableText: BlockComponentProps;
-
-console.log("Props: ",portableText)
-
-</script>
-
 <blockquote class="blockquote m-3 py-4 bg-secondary-50-900-token">
 	<slot />
 </blockquote>
