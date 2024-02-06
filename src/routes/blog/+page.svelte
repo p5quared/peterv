4f9a8beadd593376f6b1af01cd6053b44ead8471
Peter Vaiciulis <petervaiciulis@gmail.com>
2024-02-05 22:45:14 -0500

    reformat blog browsing

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,36 +1,40 @@
 <script lang="ts">
-import type { PageData } from "./$types";
-export let data: PageData
+	import type { PageData } from './$types';
+	export let data: PageData;
 
-import { page } from "$app/stores";
-
-const { posts } = data
+	import { page } from '$app/stores';
+	const { posts } = data;
 </script>
 
 <svelte:head>
 	<title>peterv! | Blog</title>
 </svelte:head>
 
-
 <div>
-{#if posts && posts.length}
-<ul class="space-y-16">
-	{#each posts as post}
-	<li>
-	<p class="text-secondary-800-100-token">{post._createdAt}</p>
-	<ul>
-		{#each post.tags as tag}
-		<li class="inline-block mr-2 text-tertiary-600-300-token">
-			{tag}
-		</li>
-		{/each}
-	<ul>
-	<a class="anchor h2"
-	href={`${$page.url.pathname}/${post.slug.current}`}>{post.title}</a>
-	</li>
-	{/each}
-</ul>
-{:else}
-	<p>No posts found</p>
-{/if}
+	{#if posts && posts.length}
+		<ul class="space-y-16">
+			{#each posts as post}
+				<li>
+					<a class="anchor h2" href={`${$page.url.pathname}/${post.slug.current}`}>
+						{post.title}
+					</a>
+					<div class="flex gap-8">
+					<p class="text-secondary-500-400-token">{post._createdAt}</p>
+					<ul>
+						{#each post.tags as tag}
+							<li class="inline-block mr-2 text-tertiary-600-300-token">
+								{tag}
+							</li>
+						{/each}
+					</ul>
+					</div>
+					{#if post.ogDescription}
+						<p class="text-secondary-900-50-token">{post.ogDescription}</p>
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	{:else}
+		<p>No posts found</p>
+	{/if}
 </div>
