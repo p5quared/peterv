024d820baa15151584ff8014fe04dd80f4ec10f7
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-07-13 11:25:00 -0400

    article containers and better readability

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -21,7 +21,8 @@
 
 
 <h1 class="h1 pb-2">{post.title}</h1>
-<article class="prose [&>p]:leading-relaxed text-inherit [&>h2]:text-inherit [&>h3]:text-inherit [&>h4]:text-inherit">
+<article class="prose lg:prose-lg [&>p]:leading-relaxed text-inherit [&>h2]:text-inherit [&>h3]:text-inherit [&>h4]:text-inherit
+	lg:p-8 lg:mt-4 lg:shadow-encore lg:border-2 lg:border-primary-900-50-token">
 	<PortableRenderer value={post.body} />
 	<p>-Peter V.</p>
 </article>
