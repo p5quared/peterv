c7977300f3060d79332f891a21d889b02c58d09d
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-07-12 20:05:58 -0400

    test broser clg

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -1,3 +1,4 @@
+import { browser } from '$app/environment';
 import { sanityClient } from '$lib/sanityClient';
 import type { PageLoad } from './$types'
 import { error } from '@sveltejs/kit'
@@ -5,8 +6,12 @@ import { error } from '@sveltejs/kit'
 export const load = (async () => {
 	const posts = await sanityClient.fetch(`*[_type =="post"] | order(_createdAt desc)`);
 
+	if (browser)
+		console.log("BROWSER")
+
 	if (posts) {
 		return { posts };
 	}
+
 	throw error(500, 'Internal Server Error')
 }) satisfies PageLoad
