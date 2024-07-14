16eb6c1ece204822d981f99ff373349798ded185
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-07-14 16:44:18 -0400

    remove skeleton

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,45 @@
-import { join } from 'path'
 import typography from '@tailwindcss/typography'
-import { skeleton } from '@skeletonlabs/tw-plugin'
-
-import { myTheme } from './custom-theme.ts'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}',
-		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			boxShadow: {
-				'encore': '5px 5px 0 0 black'
+				'encore': '5px 5px 1px #011627',
+				'encore-sm': '3px 3px #011627'
+			},
+			fontFamily: {
+				'sans': ['roboto', ...defaultTheme.fontFamily.sans],
+				'serif': ['ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', ...defaultTheme.fontFamily.serif],
+			},
+			fontSize: {
+				base: ''
+			},
+			colors: {
+				'background': '#FDFDF9',
+				'text-primary': '#011627',
+				'links': '#3E885B',
+				'tag': '#FF9F1C'
+			},
+			typography: {
+				DEFAULT: {
+					css: {
+						color: '#011627',
+						a: {
+							color: '#3E885B',
+							'&:hover': {
+								filter: 'brightness(1.5)',
+							},
+						},
+						'blockquote p:first-of-type::before': false,
+						'blockquote p:last-of-type::after': false,
+					},
+				}
 			}
 		},
 	},
-	plugins: [typography,
-		skeleton({
-			themes: {
-				preset: ["skeleton", 'crimson', 'modern'],
-				custom: [myTheme],
-			}
-		})
-	],
+	plugins: [typography],
 }
