9778aaa3bfad26c7312f83b2c4b890fb9fd62d4d
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-08-29 22:18:20 -0700

    stub out audioplayer

--- src/lib/PortableRenderer.svelte
+++ src/lib/PortableRenderer.svelte
@@ -6,15 +6,18 @@
 	import PortableImage from './PortableImage.svelte';
 	import PortableAnchor from './PortableAnchor.svelte';
 	import PortableBlockQuote from './PortableBlockQuote.svelte';
+	import PortableAudioPlayer from './PortableAudioPlayer.svelte';
 
 	export let value: InputValue;
+	console.log(value)
 </script>
 
 <PortableText
 	{value}
 	components={{
 		types: {
-			image: PortableImage
+			image: PortableImage,
+			audioPlayer: PortableAudioPlayer
 		},
 		marks: {
 			link: PortableAnchor
