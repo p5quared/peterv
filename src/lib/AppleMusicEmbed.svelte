bbe54e38978d6c04e6edeacda911fb092f534374
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2024-09-11 21:10:06 -0700

    feat: apple music embed

---
+++ src/lib/AppleMusicEmbed.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let src;
+	const base = "https://embed.music.apple.com/us/album/" 
+	src = base + src
+</script>
+<iframe 
+	class="min-w-fit shadow-encore"
+	height="175"
+	width="100%" 
+	title="Media player" 
+	src={src}
+	id="embedPlayer" 
+	style="border:0;border-radius:12px;width:100%;height:175px;max-width:660px" 
+	sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" 
+	allow="autoplay *; encrypted-media *; clipboard-write">
+</iframe>
